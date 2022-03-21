8dda07de960bc32563f8b6806069e507dc775eb9
Tatiane Tavares Masutti <73369429+tatimasutti@users.noreply.github.com>
2022-03-21 12:23:02 -0300

    Update adivinha.py

--- Python/adivinha.py
+++ Python/adivinha.py
@@ -9,7 +9,7 @@ contador = 0
 
 print("_______________________________________\n")
 print("Bem vindo a Adivinhação!\n")
-print('Será sorteado um número entre 1 e 50!\n')
+print('Será sorteado um número entre 1 e 100!\n')
 print("Você tem 5 tentativas válidas, ou seja,\n\
 se chutar um número fora do intervalo\n\
 ou letras e símbolos não contam.")
@@ -17,7 +17,7 @@ ou letras e símbolos não contam.")
 while contador < 5: #Se a quantidade de chutes chegar a 5 encerra o while.
     try: #Utilizado para testar se é um número válido
         print("_______________________________________")
-        chute = int(input("\nDigite um número entre 1 e 50: "))
+        chute = int(input("\nDigite um número entre 1 e 100: "))
         if (0 < chute <= 100): #Se o chute for um valor válido.
             contador = contador + 1 #Define o quantidade de tentativas.
             restante = 5 - contador #Define quantas tentativas restam.
